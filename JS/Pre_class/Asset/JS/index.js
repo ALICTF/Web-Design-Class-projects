be4c504af6b11240eca56691a5c066bb65fdedc4
ALICTF <alictf1@GMAIL.COM>
2023-12-29 11:15:38 +0330

    practice & , || , null , infinite , check

---
+++ JS/Pre_class/Asset/JS/index.js
@@ -0,0 +1,32 @@
+// let x = Number(prompt("adad ro vared konid : " , "example : 12"))
+// let y = Number(prompt("adad ro vared konid : " , "example : 12"))
+// c = x+y
+// console.log(c);
+// document.write(`<h1> majmo adad vared shode shoma barabar as ba ${c} </h1>`)
+
+
+// &&
+// result = x==10 && y==20
+// console.log(typeof(result));
+// console.log(result);
+
+
+// ||
+// result = x==10 || y==20
+// console.log(typeof(result));
+// console.log(result);
+
+
+//nul empty
+// let g = null
+// console.log(g);
+// console.log(typeof(g));
+
+
+// infinity
+// let div = x/y
+// console.log(isfinite (div))
+// console.log(typeof(div));
+
+let check = confirm("do you want ...?")
+console.log(check);
