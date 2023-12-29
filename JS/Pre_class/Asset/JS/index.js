e7d0cf58e9a6e0728ce03babf3b8f1427253eb6f
ALICTF <alictf1@GMAIL.COM>
2023-12-29 15:00:45 +0330

    Practice if , else if , ...

--- JS/Pre_class/Asset/JS/index.js
+++ JS/Pre_class/Asset/JS/index.js
@@ -28,5 +28,86 @@
 // console.log(isfinite (div))
 // console.log(typeof(div));
 
-let check = confirm("do you want ...?")
-console.log(check);
+
+// let check = confirm("do you want ...?")
+// console.log(check);
+
+
+// debugger
+// age = Number(prompt("senet ro bego "))
+// if(age>=18){
+//     console.log("you are adult");
+// }
+// else{
+//     console.log("you are kid");
+// }
+// console.log('dobare say kon');
+
+
+
+// let username = prompt("meghdare user")
+// let password = prompt("meghdare pass")
+// if(username=="admin" && password=="123456"){
+//     alert("login successfull");
+// }
+// else{
+//     alert("wrong info");
+// }
+
+// let username = prompt("meghdare user")
+// let password = prompt("meghdare pass")
+// if(username=="admin"){
+//     if(password=="123456"){
+//     alert("login successfull");
+// }
+// }
+// else{
+//     alert("wrong info");
+// }
+
+
+// greater of 3 numbers with if 
+// x = Number(prompt("adad aval :"))
+// y = Number(prompt("adad dovom :"))
+// z = Number(prompt("adad sevom :"))
+// if(x>y && x>z){
+//     document.write("greater is "+x)
+// }
+// else if (y>x && y>z){
+//     document.write("greater is "+y)
+// }
+// else {
+//     document.write("greater is "+z)
+// }
+
+
+// mosalas
+// x = Number(prompt("adad aval :"))
+// y = Number(prompt("adad dovom :"))
+// z = Number(prompt("adad sevom :"))
+// if (x*x == y*y + z*z || y*y == x*x + z*z || z*z == x*x + y*y ){
+//     alert("mosalas qaemo al zavioye hast");
+// }
+// else{
+//     alert("mosalas qaemo az zavioye nist");
+// }
+
+
+// x,y opperand +,-,/,*
+// x = Number(prompt("enter the first number :"))
+// y = Number(prompt("enter the secound number :"))
+// opper = prompt("enter your oprator :(+ or / or - or * )")
+
+// if (opper == "+"){
+//     alert(x+y)
+// }else if (opper == "-"){
+//     alert(x-y)
+// }else if (opper == "*"){
+//     alert(x*y)
+// }else if (opper == "/"){
+//     alert(x/y)
+// }else {
+//     alert("please enter correct value")
+// }
+
+
