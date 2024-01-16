07cbaa8746ebb78dd62fcf87bce86bb4ca040a2a
Mostafa Khajenezhad <143581356+ALICTF@users.noreply.github.com>
2024-01-16 16:08:40 +0330

    fixing bugs and "." btn

--- JS/Class_projects/Asset/JS/calculator.js
+++ JS/Class_projects/Asset/JS/calculator.js
@@ -1,49 +1,46 @@
+var firstNumber;
+var secondNumber;
+var flagNumb = false
 const mClickNumb = (numb) => {
-    document.querySelector("input").value == 0 ?
-    document.querySelector("input").value = numb:
-    document.querySelector("input").value += numb;
+    debugger
+    if(document.querySelector("input").value == "0" &&  flagNumb == false){
+        document.querySelector("input").value = numb;
+        flagNumb = true;
+    } 
+    else {
+        document.querySelector("input").value += numb;
+    }
 }
 const Clear = () => {document.querySelector("input").value = 0
 };
 mClickOpra = (opra) => {
-    if (opra == '+' || opra == '='){
-        var firstNumber = Number(document.querySelector('input').value);
+    debugger
+    if (opra == '+' ){
+        firstNumber = Number(document.querySelector('input').value);
         document.querySelector("input").value = 0
-        console.log(firstNumber);
-        mClickOpra =(opra)=>{
-            if(opra== '=' )
+        flagNumb = false 
+    }       
+    if(opra== '=' ){          
             var secondNumber = Number(document.querySelector('input').value)
-            document.querySelector("input").value = firstNumber + secondNumber
-        }
+            document.querySelector("input").value = firstNumber + secondNumber}
+    
     }
-    if (opra == '-' || opra == '='){
-        var firstNumber = Number(document.querySelector('input').value);
-        document.querySelector("input").value = 0
-        mClickOpra =(opra)=>{
-            if(opra== '=' )
-            var secondNumber = Number(document.querySelector('input').value);
-            document.querySelector("input").value = firstNumber - secondNumber
-    }
-    }
-    if (opra == '*' || opra == '='){
-        var firstNumber = Number(document.querySelector('input').value);
-        document.querySelector("input").value = 0
-        mClickOpra =(opra)=>{
-                if(opra== '=' )
-                var secondNumber = Number(document.querySelector('input').value);
-                document.querySelector("input").value = firstNumber * secondNumber
-            }
-            }
-            if (opra == '/' || opra == '='){
-                var firstNumber = Number(document.querySelector('input').value);
-                document.querySelector("input").value = 0
-                mClickOpra =(opra)=>{
-                        if(opra== '=' )
-                        var secondNumber = Number(document.querySelector('input').value);
-                        document.querySelector("input").value = firstNumber / secondNumber
-                    }
-                    }
-    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 document.querySelector("body").addEventListener("keyup", (e) => {
 console.log(e.key);
 if (e.key >= "0" && e.key <= "9") {
@@ -54,3 +51,56 @@ if (e.key >= "0" && e.key <= "9") {
     Clear();
 }
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// if (opra == '-' || opra == '='){
+//     var firstNumber = Number(document.querySelector('input').value);
+//     document.querySelector("input").value = 0
+//     mClickOpra =(opra)=>{
+//         if(opra== '=' )
+//         var secondNumber = Number(document.querySelector('input').value);
+//         document.querySelector("input").value = firstNumber - secondNumber
+// }
+// }
+// if (opra == '*' || opra == '='){
+//     var firstNumber = Number(document.querySelector('input').value);
+//     document.querySelector("input").value = 0
+//     mClickOpra =(opra)=>{
+//             if(opra== '=' )
+//             var secondNumber = Number(document.querySelector('input').value);
+//             document.querySelector("input").value = firstNumber * secondNumber
+//         }
+//         }
+// if (opra == '/' || opra == '='){
+//             var firstNumber = Number(document.querySelector('input').value);
+//             document.querySelector("input").value = 0
+//             mClickOpra =(opra)=>{
+//                     if(opra== '=' )
+//                     var secondNumber = Number(document.querySelector('input').value);
+//                     document.querySelector("input").value = firstNumber / secondNumber
+//                 }
+//                 }
