c9f5a8056176aad5dd47acc96ae85743579ea7dc
Mostafa Khajenezhad <143581356+ALICTF@users.noreply.github.com>
2024-01-17 14:48:23 +0330

    js done

--- JS/Class_projects/Asset/JS/ligenerator.js
+++ JS/Class_projects/Asset/JS/ligenerator.js
@@ -1,13 +1,19 @@
-var ulTag = document.getElementById("ulTag")
+var flag = false
 function clickBtn(){
-
     let userUl = Number(prompt("enter the number of li .... "))
+    // if (flag == true ){
+    //     document.querySelector("ul").remove()
+    //     document.getElementById("div").appendChild(document.createElement("ul")) 
+    //     alert("ul sakhte shod")
+    //     flag=false
+    //     }
     for(let i = 1 ;i<=userUl;i++){
-        ulTag.appendChild(document.createElement("li"))
-        document.getElementsByTagName("li").innerText = i
+        document.querySelector("ul").appendChild(document.createElement("li")).innerHTML =`it is li number : ${i}`
+        
     }
-    
-    }
+    flag=true
+}
+
 
 
 
