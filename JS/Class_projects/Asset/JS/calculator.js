be2c6d7211945d2b87d702ba55e927f777dbf9d1
Mostafa Khajenezhad <143581356+ALICTF@users.noreply.github.com>
2024-01-17 15:49:56 +0330

    calc done

--- JS/Class_projects/Asset/JS/calculator.js
+++ JS/Class_projects/Asset/JS/calculator.js
@@ -1,106 +1,93 @@
 var firstNumber;
 var secondNumber;
-var flagNumb = false
+var opraType;
+var flagNumb = false;
 const mClickNumb = (numb) => {
-    debugger
-    if(document.querySelector("input").value == "0" &&  flagNumb == false){
+    debugger;
+    if (document.querySelector("input").value == "0" && flagNumb == false) {
         document.querySelector("input").value = numb;
         flagNumb = true;
-    } 
-    else {
+    } else {
         document.querySelector("input").value += numb;
     }
-}
-const Clear = () => {document.querySelector("input").value = 0
+};
+const Clear = () => {
+    document.querySelector("input").value = 0;
+    debugger;
+    flagNumb = false;
 };
 mClickOpra = (opra) => {
-    debugger
-    if (opra == '+' ){
-        firstNumber = Number(document.querySelector('input').value);
-        document.querySelector("input").value = 0
-        flagNumb = false 
-    }       
-    if(opra== '=' ){          
-            var secondNumber = Number(document.querySelector('input').value)
-            document.querySelector("input").value = firstNumber + secondNumber}
-    
+    debugger;
+    if (opra == "+") {
+        firstNumber = Number(document.querySelector("input").value);
+        document.querySelector("input").value = 0;
+        flagNumb = false;
+        opraType = opra;
+    }
+    if (opra == "-") {
+        firstNumber = Number(document.querySelector("input").value);
+        document.querySelector("input").value = 0;
+        flagNumb = false;
+        opraType = opra;
+    }
+    if (opra == "*") {
+        firstNumber = Number(document.querySelector("input").value);
+        document.querySelector("input").value = 0;
+        flagNumb = false;
+        opraType = opra;
+    }
+    if (opra == "/") {
+        firstNumber = Number(document.querySelector("input").value);
+        document.querySelector("input").value = 0;
+        flagNumb = false;
+        opraType = opra;
+    }
+    if (opra == "%") {
+        firstNumber = Number(document.querySelector("input").value);
+        document.querySelector("input").value = 0;
+        flagNumb = false;
+        opraType = opra;
+    }
+};
+
+function cliclmm(opraa) {
+    if (opraa == "=" && opraType == "+") {
+        var secondNumber = Number(document.querySelector("input").value);
+        document.querySelector("input").value = firstNumber + secondNumber;
+    }
+    if (opraa == "=" && opraType == "-") {
+        var secondNumber = Number(document.querySelector("input").value);
+        document.querySelector("input").value = firstNumber - secondNumber;
     }
 
+    if (opraa == "=" && opraType == "*") {
+        var secondNumber = Number(document.querySelector("input").value);
+        document.querySelector("input").value = firstNumber * secondNumber;
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+    if (opraa == "=" && opraType == "/") {
+        var secondNumber = Number(document.querySelector("input").value);
+        document.querySelector("input").value = firstNumber / secondNumber;
+    }
+    if (opraa == "=" && opraType == "%") {
+        var secondNumber = Number(document.querySelector("input").value);
+        document.querySelector("input").value = firstNumber % secondNumber;
+    }
+}
 
 document.querySelector("body").addEventListener("keyup", (e) => {
-console.log(e.key);
-if (e.key >= "0" && e.key <= "9") {
-    mClickNumb(Number(e.key));
-} else if (e.key == "+" || e.key == "-" || e.key == "*" || e.key == "/" || e.key == "=")  {
-    mClickOpra(e.key);
-} else if (e.key == "c") {
-    Clear();
-}
+    console.log(e.key);
+    if (e.key >= "0" && e.key <= "9") {
+        mClickNumb(Number(e.key));
+    } else if (
+        e.key == "+" ||
+        e.key == "-" ||
+        e.key == "*" ||
+        e.key == "/" ||
+        e.key == "="
+    ) {
+        mClickOpra(e.key);
+    } else if (e.key == "c") {
+        Clear();
+    }
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// if (opra == '-' || opra == '='){
-//     var firstNumber = Number(document.querySelector('input').value);
-//     document.querySelector("input").value = 0
-//     mClickOpra =(opra)=>{
-//         if(opra== '=' )
-//         var secondNumber = Number(document.querySelector('input').value);
-//         document.querySelector("input").value = firstNumber - secondNumber
-// }
-// }
-// if (opra == '*' || opra == '='){
-//     var firstNumber = Number(document.querySelector('input').value);
-//     document.querySelector("input").value = 0
-//     mClickOpra =(opra)=>{
-//             if(opra== '=' )
-//             var secondNumber = Number(document.querySelector('input').value);
-//             document.querySelector("input").value = firstNumber * secondNumber
-//         }
-//         }
-// if (opra == '/' || opra == '='){
-//             var firstNumber = Number(document.querySelector('input').value);
-//             document.querySelector("input").value = 0
-//             mClickOpra =(opra)=>{
-//                     if(opra== '=' )
-//                     var secondNumber = Number(document.querySelector('input').value);
-//                     document.querySelector("input").value = firstNumber / secondNumber
-//                 }
-//                 }
