3d28d2d1347e35aed768ae1c51671435197961c3
Mostafa Khajenezhad <143581356+ALICTF@users.noreply.github.com>
2024-01-18 14:46:09 +0330

    li generator replace tag fixed

--- JS/Class_projects/Asset/JS/ligenerator.js
+++ JS/Class_projects/Asset/JS/ligenerator.js
@@ -1,17 +1,17 @@
 var flag = false
 function clickBtn(){
     let userUl = Number(prompt("enter the number of li .... "))
-    // if (flag == true ){
-    //     document.querySelector("ul").remove()
-    //     document.getElementById("div").appendChild(document.createElement("ul")) 
-    //     alert("ul sakhte shod")
-    //     flag=false
-    //     }
+    if (flag == true ){
+        document.querySelector("ul").remove()
+        document.getElementById("div").appendChild(document.createElement("ul")) 
+        
+        flag=false
+        }
     for(let i = 1 ;i<=userUl;i++){
         document.querySelector("ul").appendChild(document.createElement("li")).innerHTML =`it is li number : ${i}`
         
     }
-    // flag=true
+    flag=true
 }
 
 
